f30571be8d5ac6a099752c8935aec231a302afdb
ZhouTian0716 <egglord0716@gmail.com>
2022-12-31 10:30:10 +1100

    responsive nav

---
+++ note.md
@@ -0,0 +1,4 @@
+笔记：
+1.tailwindcss 官网查看vite的setup步骤
+2.tailwind extend config， huemin查看颜色方案
+3.
--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
+import { useEffect, useState } from "react";
+import { SelectedPage } from "@/shared/types";
 
-
+import Navbar from "@/scenes/navbar";
 
 function App() {
-  
-
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home
+  );
   return (
-    <div className="app">
-      app
+    <div className="app bg-gray-20">
+      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
